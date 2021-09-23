a7900d1d0b05b92deea94b545e5e5b1e7a8dd884
SMBenedict173 <60229667+SMBenedict173@users.noreply.github.com>
2021-09-23 09:55:56 -0500

    Update ValveDial.cs
    Now to have dials and valves rotate, an animation must be set for that object, which can be a bit time consuming.

--- GeneralAnesthesiaSimulator/Assets/Scripts/ValveDial.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/ValveDial.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using UnityEngine;
 
-public class ValveDial : Grippable
+public class ValveDial : Grippable, IToggleable
 {
+    [Tooltip("Animator")]
+    public Animator animator;
     public float OffRotation;
     public float OnRotation;
     private float currentRotation;
+    private float targetRotation;
 
+    private string rotateAnimationParameter = "Rotate";
+    public float AnimationDelta;
 	void Update()
     {
-        if (IsActivated && currentRotation == OffRotation)
+        //Attempt without using animations
+        //if (IsActivated && currentRotation == OffRotation)
+        //{
+        //    ThisTransform.rotation.Set(0, OnRotation, 0, 0);
+        //    currentRotation = OnRotation;
+        //}
+        //else if (!IsActivated && currentRotation == OnRotation)
+        //{
+        //    ThisTransform.rotation.Set(0, OffRotation, 0, 0);
+        //    currentRotation = OnRotation;
+        //}
+
+        //Attempt using animations...Requires all dials and valves to have animations
+        if (currentRotation != targetRotation)
         {
-            ThisTransform.rotation.Set(0, OnRotation, 0, 0);
-            currentRotation = OnRotation;
+            currentRotation = Mathf.MoveTowards(currentRotation, targetRotation, Time.time * AnimationDelta);
+            animator.SetFloat(rotateAnimationParameter, currentRotation);
         }
-        else if (!IsActivated && currentRotation == OnRotation)
+    }
+     
+    public void ToggleActivation()
+    {
+        if (IsActivated)
         {
-            ThisTransform.rotation.Set(0, OffRotation, 0, 0);
-            currentRotation = OnRotation;
+            IsActivated = false;
+            targetRotation = OffRotation;
+        }
+        else
+        {
+            IsActivated = true;
+            targetRotation = OnRotation;
         }
     }
 }
