e55bb8de9244491e32792e29e7eaaa1afce82e6f
SMBenedict173 <60229667+SMBenedict173@users.noreply.github.com>
2021-10-26 07:37:52 -0500

    changed a bit of code to hopefully work.

--- GeneralAnesthesiaSimulator/Assets/Scripts/MenuManager.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/MenuManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public bool IsMenuOpen { get; private set; }
-    public Canvas MenuCanvas;
+    public GameObject menuRoot;
     //public SimulationFlowManager SimulationFlowManager; Not ready yet.
     // Start is called before the first frame update
     void Start()
     {
-        this.IsMenuOpen = false; 
+        this.IsMenuOpen = false;
+        this.menuRoot.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,14 +23,14 @@ public class MenuManager : MonoBehaviour
     public void OpenMenu()
     {
         IsMenuOpen = true;
-        this.MenuCanvas.enabled = true;
+        this.menuRoot.SetActive(true);
         //this.SimulationFlowManager.StopTimer(); Not ready yet
     }
 
     public void CloseMenu()
     {
         IsMenuOpen = true;
-        this.MenuCanvas.enabled = false;
+        this.menuRoot.SetActive(false);
         //this.SimulationFlowManager.RestartTimer(); Not ready yet
     }
 }
