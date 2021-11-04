2e8c5aff20eec286a07af994add74588c5354f87
Sawyer_Emerson <semerso2@samford.edu>
2021-11-04 12:20:58 -0500

    added serialized lists for interactions, updated safetyGuideText indexing options

---
+++ GeneralAnesthesiaSimulator/Assets/Scripts/SafetyGuideInteractions.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyGuideInteractions : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> section1step1;
+
+    [SerializeField]
+    List<GameObject> section2step1sub1;
+
+    [SerializeField]
+    List<GameObject> section2step1sub2;
+
+    [SerializeField]
+    List<GameObject> section2step2;
+
+    [SerializeField]
+    List<GameObject> section3step1sub1;
+
+    [SerializeField]
+    List<GameObject> section3step1sub2;
+
+    [SerializeField]
+    List<GameObject> section3step2sub1;
+
+    [SerializeField]
+    List<GameObject> section3step2sub2;
+
+    [SerializeField]
+    List<GameObject> section3step2sub3;
+
+    [SerializeField]
+    List<GameObject> section3step2sub4;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub1;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub2;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub3;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub4;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub5;
+
+    [SerializeField]
+    List<GameObject> section3step2sub5subsub6;
+
+    [SerializeField]
+    List<GameObject> section3step2sub6;
+
+    [SerializeField]
+    List<GameObject> section3step3;
+
+    [SerializeField]
+    List<GameObject> section3step4sub1;
+
+    [SerializeField]
+    List<GameObject> section3step4sub2;
+
+    [SerializeField]
+    List<GameObject> section4step1sub1;
+
+    [SerializeField]
+    List<GameObject> section4step1sub2;
+
+    [SerializeField]
+    List<GameObject> section4step1sub3;
+
+    [SerializeField]
+    List<GameObject> section4step1sub4;
+
+    [SerializeField]
+    List<GameObject> section4step1sub5;
+
+    [SerializeField]
+    List<GameObject> section5step1sub1;
+
+    [SerializeField]
+    List<GameObject> section5step1sub2;
+
+    [SerializeField]
+    List<GameObject> section5step1sub3;
+
+    [SerializeField]
+    List<GameObject> section5step1sub4;
+
+    [SerializeField]
+    List<GameObject> section5step2sub1;
+
+
+    [SerializeField]
+    List<GameObject> section5step2sub2;
+
+    [SerializeField]
+    List<GameObject> section5step2sub3;
+
+    [SerializeField]
+    List<GameObject> section5step2sub4;
+
+    [SerializeField]
+    List<GameObject> section5step3sub1;
+
+    [SerializeField]
+    List<GameObject> section5step3sub2;
+
+    [SerializeField]
+    List<GameObject> section5step3sub3;
+
+    [SerializeField]
+    List<GameObject> section5step3sub4;
+
+    [SerializeField]
+    List<GameObject> section5step3sub5;
+
+    [SerializeField]
+    List<GameObject> section6step1sub1;
+
+    [SerializeField]
+    List<GameObject> section6step1sub2;
+
+    [SerializeField]
+    List<GameObject> section6step1sub3;
+
+    [SerializeField]
+    List<GameObject> section6step1sub4;
+
+    [SerializeField]
+    List<GameObject> section6step1sub5;
+
+    [SerializeField]
+    List<GameObject> section6step1sub6;
+
+    [SerializeField]
+    List<GameObject> section6step1sub7;
+
+    [SerializeField]
+    List<GameObject> section6step1sub8;
+
+    [SerializeField]
+    List<GameObject> section6step1sub9;
+
+    [SerializeField]
+    List<GameObject> section6step1sub10;
+
+    [SerializeField]
+    List<GameObject> section6step1sub11;
+
+    [SerializeField]
+    List<GameObject> section6step1sub12;
+
+    [SerializeField]
+    List<GameObject> section6step1sub13;
+
+    [SerializeField]
+    List<GameObject> section7step1;
+
+    [SerializeField]
+    List<GameObject> section8step1sub1;
+
+    [SerializeField]
+    List<GameObject> section8step1sub2;
+
+    [SerializeField]
+    List<GameObject> section8step1sub3;
+
+    [SerializeField]
+    List<GameObject> section8step1sub4;
+
+    [SerializeField]
+    List<GameObject> section8step1sub5;
+
+    [SerializeField]
+    List<GameObject> section8step1sub6;
+
+
+
+
+}
