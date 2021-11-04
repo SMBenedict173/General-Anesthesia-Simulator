5d3ab5e7fa65901b5595dd7aa1e65f90dcf0a8f0
Sawyer_Emerson <semerso2@samford.edu>
2021-11-04 12:49:10 -0500

    re-did the interactions class to support nested lists

--- GeneralAnesthesiaSimulator/Assets/Scripts/SafetyGuideInteractions.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/SafetyGuideInteractions.cs
@@ -5,181 +5,26 @@ using UnityEngine;
 public class SafetyGuideInteractions : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> section1step1;
+    public List<serializableSection> SafetySections = new List<serializableSection>();
+}
 
+[System.Serializable]
+public class serializableSection
+{
     [SerializeField]
-    List<GameObject> section2step1sub1;
+    public List<serializableStep> steps = new List<serializableStep>();
+}
 
+[System.Serializable]
+public class serializableStep
+{
     [SerializeField]
-    List<GameObject> section2step1sub2;
+    public List<serializableSubstep> substeps;
+}
 
+[System.Serializable]
+public class serializableSubstep
+{
     [SerializeField]
-    List<GameObject> section2step2;
-
-    [SerializeField]
-    List<GameObject> section3step1sub1;
-
-    [SerializeField]
-    List<GameObject> section3step1sub2;
-
-    [SerializeField]
-    List<GameObject> section3step2sub1;
-
-    [SerializeField]
-    List<GameObject> section3step2sub2;
-
-    [SerializeField]
-    List<GameObject> section3step2sub3;
-
-    [SerializeField]
-    List<GameObject> section3step2sub4;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub1;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub2;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub3;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub4;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub5;
-
-    [SerializeField]
-    List<GameObject> section3step2sub5subsub6;
-
-    [SerializeField]
-    List<GameObject> section3step2sub6;
-
-    [SerializeField]
-    List<GameObject> section3step3;
-
-    [SerializeField]
-    List<GameObject> section3step4sub1;
-
-    [SerializeField]
-    List<GameObject> section3step4sub2;
-
-    [SerializeField]
-    List<GameObject> section4step1sub1;
-
-    [SerializeField]
-    List<GameObject> section4step1sub2;
-
-    [SerializeField]
-    List<GameObject> section4step1sub3;
-
-    [SerializeField]
-    List<GameObject> section4step1sub4;
-
-    [SerializeField]
-    List<GameObject> section4step1sub5;
-
-    [SerializeField]
-    List<GameObject> section5step1sub1;
-
-    [SerializeField]
-    List<GameObject> section5step1sub2;
-
-    [SerializeField]
-    List<GameObject> section5step1sub3;
-
-    [SerializeField]
-    List<GameObject> section5step1sub4;
-
-    [SerializeField]
-    List<GameObject> section5step2sub1;
-
-
-    [SerializeField]
-    List<GameObject> section5step2sub2;
-
-    [SerializeField]
-    List<GameObject> section5step2sub3;
-
-    [SerializeField]
-    List<GameObject> section5step2sub4;
-
-    [SerializeField]
-    List<GameObject> section5step3sub1;
-
-    [SerializeField]
-    List<GameObject> section5step3sub2;
-
-    [SerializeField]
-    List<GameObject> section5step3sub3;
-
-    [SerializeField]
-    List<GameObject> section5step3sub4;
-
-    [SerializeField]
-    List<GameObject> section5step3sub5;
-
-    [SerializeField]
-    List<GameObject> section6step1sub1;
-
-    [SerializeField]
-    List<GameObject> section6step1sub2;
-
-    [SerializeField]
-    List<GameObject> section6step1sub3;
-
-    [SerializeField]
-    List<GameObject> section6step1sub4;
-
-    [SerializeField]
-    List<GameObject> section6step1sub5;
-
-    [SerializeField]
-    List<GameObject> section6step1sub6;
-
-    [SerializeField]
-    List<GameObject> section6step1sub7;
-
-    [SerializeField]
-    List<GameObject> section6step1sub8;
-
-    [SerializeField]
-    List<GameObject> section6step1sub9;
-
-    [SerializeField]
-    List<GameObject> section6step1sub10;
-
-    [SerializeField]
-    List<GameObject> section6step1sub11;
-
-    [SerializeField]
-    List<GameObject> section6step1sub12;
-
-    [SerializeField]
-    List<GameObject> section6step1sub13;
-
-    [SerializeField]
-    List<GameObject> section7step1;
-
-    [SerializeField]
-    List<GameObject> section8step1sub1;
-
-    [SerializeField]
-    List<GameObject> section8step1sub2;
-
-    [SerializeField]
-    List<GameObject> section8step1sub3;
-
-    [SerializeField]
-    List<GameObject> section8step1sub4;
-
-    [SerializeField]
-    List<GameObject> section8step1sub5;
-
-    [SerializeField]
-    List<GameObject> section8step1sub6;
-
-
-
-
+    public List<GameObject> interactions;
 }
