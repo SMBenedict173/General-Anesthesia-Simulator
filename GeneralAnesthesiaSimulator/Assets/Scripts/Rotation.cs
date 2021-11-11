54cc949b25a09fb51d224157e1068683c781942e
Sawyer_Emerson <semerso2@samford.edu>
2021-11-11 11:56:50 -0600

    updated rotation

--- GeneralAnesthesiaSimulator/Assets/Scripts/Rotation.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/Rotation.cs
@@ -8,6 +8,10 @@ public class Rotation : MonoBehaviour
     public bool isActivated;
     public bool clockwiseTurn;
     public float changeAmount;
+    public float minRotation;
+    public float maxRotation;
+    public char axis;
+    private float currentRotation;
 
     public void Start()
     {
@@ -17,9 +21,31 @@ public class Rotation : MonoBehaviour
 
     public void Update()
     {
-        if (isActivated && clockwiseTurn)
+        switch(axis){
+            case 'x':
+                currentRotation = gameObject.transform.eulerAngles.x;
+                break;
+            case 'y':
+                currentRotation = gameObject.transform.eulerAngles.y;
+                break;
+            case 'z':
+                currentRotation = gameObject.transform.eulerAngles.z;
+                break;
+
+        }
+
+        Debug.Log(currentRotation);
+
+
+
+        if (isActivated && clockwiseTurn && currentRotation + changeAmount < maxRotation)
             transform.Rotate(Vector3.up * changeAmount);
-        else if (isActivated && !clockwiseTurn)
+
+        else if (isActivated && !clockwiseTurn && currentRotation - changeAmount > minRotation)
             transform.Rotate(Vector3.down * changeAmount);
+
+
+
+        float test = gameObject.transform.rotation.x;
     }
 }
