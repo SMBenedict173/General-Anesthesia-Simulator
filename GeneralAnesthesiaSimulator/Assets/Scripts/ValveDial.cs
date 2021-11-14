4e0670d49b33cdaa5abefac95fc5538c09d83d99
SMBenedict173 <60229667+SMBenedict173@users.noreply.github.com>
2021-11-14 14:19:13 -0600

    Added ValveDial interactable touch points where obvious.
    Mainly the colored things with grippy textures that look like they are manually rotatable.

--- GeneralAnesthesiaSimulator/Assets/Scripts/ValveDial.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/ValveDial.cs
@@ -7,11 +7,36 @@ public class ValveDial : Toggleable
     public float OnRotation;
     private float targetRotation;
     public Transform ThisTransform;
+    public char AxisOfRotation;
 
     public float AnimationDelta;
-	void Update()
+
+    void Start()
     {
-        Quaternion newRotation = Quaternion.Euler(0, targetRotation, 0);
+        if (char.IsWhiteSpace(AxisOfRotation))
+        {
+            throw new Exception("Axis of rotation must be specified.");
+        }
+    }
+    void Update()
+    {
+        Quaternion newRotation = Quaternion.Euler(0, 0, 0);
+        switch (AxisOfRotation)
+        {
+            case 'x':
+            case 'X':
+                newRotation = Quaternion.Euler(targetRotation, 0, 0);
+                break;
+            case 'y':
+            case 'Y':
+                newRotation = Quaternion.Euler(0, targetRotation, 0);
+                break;
+            case 'z':
+            case 'Z':
+                newRotation = Quaternion.Euler(0, 0, targetRotation);
+                break;
+        }
+        
         transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * AnimationDelta);
     }
      
@@ -27,6 +52,4 @@ public class ValveDial : Toggleable
         IsActivated = true;
         targetRotation = OnRotation;
     }
-    
-    
 }
