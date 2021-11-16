4ba243e32d5f17535d567a19a103c9e13621c57b
Sawyer_Emerson <semerso2@samford.edu>
2021-11-16 17:36:37 -0600

    moved ToString methods from SafetyText to SafetyInteractions

--- GeneralAnesthesiaSimulator/Assets/Scripts/Interactable.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/Interactable.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
 
-    private bool completedInteraction;
+    private bool completedInteraction = false;
+    private bool isActive = false;
 
-
-    void Start()
+    public void CompleteInteraction()
     {
-        completedInteraction = false;
+        if (isActive)
+            this.completedInteraction = true;
     }
 
-    public void completeInteraction()
-    {
-        this.completedInteraction = true;
-    }
-
-    public bool getCompletedInteraction()
+    public bool GetCompletedInteraction()
     {
         return this.completedInteraction;
+    }
+
+    public void SetActive()
+    {
+        isActive = true;
+    }
+
+    public void SetInactive()
+    {
+        isActive = false;
     }
 }
--- GeneralAnesthesiaSimulator/Assets/Scripts/SafetyGuideInteractions.cs
+++ GeneralAnesthesiaSimulator/Assets/Scripts/SafetyGuideInteractions.cs
@@ -115,15 +115,54 @@ public class SafetyGuideInteractions : MonoBehaviour
         return Sections.All(a => a.isCompleted == true);
     }
 
+    override public string ToString()
+    {
+        string s = "";
+
+        foreach (var section in Sections)
+        {
+            s += "\n" + section.ToString();
+        }
+
+        return s;
+    }
+
+    public string ToStringFormatted()
+    {
+        string s = "";
+
+        foreach (var section in Sections)
+        {
+            if (!section.isCompleted)
+                s += "\n" + section.ToStringFormatted();
+            else
+                s += "\n<color=green>" + section.ToStringFormatted() + "</color>";
+        }
+
+        return s;
+    }
+
+    public string GetSectionString(int index)
+    {
+        return Sections[index].ToString();
+    }
+
+    public string GetSectionStringFormatted(int index)
+    {
+        return Sections[index].ToStringFormatted();
+    }
+
 }
 
 [System.Serializable]
 public class SerializableSection
 {
-    [SerializeField]
-    public List<SerializableStep> Steps = new List<SerializableStep>();
+
     public bool isCompleted;
     public string title;
+
+    [SerializeField]
+    public List<SerializableStep> Steps = new List<SerializableStep>();
 
     public SerializableSection(string title, List<SerializableStep> steps)
     {
@@ -139,15 +178,58 @@ public class SerializableSection
         return isCompleted;
 
     }
+
+    public string GetCurrentStep()
+    {
+        foreach (var step in Steps)
+        {
+            if (!step.isCompleted)
+                return step.description;
+        }
+
+        return "Completed Section";
+    }
+
+    override public string ToString()
+    {
+        string s = "<b>" + title + "</b>";
+
+        foreach (var step in Steps)
+        {
+            s += "\n\n" + (Steps.IndexOf(step) + 1).ToString() + ". " + step.ToString();
+        }
+
+        return s;
+    }
+
+    public string ToStringFormatted()
+    {
+        string s = "";
+
+        if (!isCompleted)
+            s += "<b>" + title + "</b>";
+
+        if (isCompleted)
+            s += "<color=green><b>" + title + "</color></b>";
+
+        foreach (var step in Steps)
+        {
+            s += "\n\n" + (Steps.IndexOf(step) + 1).ToString() + ". " + step.ToStringFormatted();
+
+        }
+
+        return s;
+    }
 }
 
 [System.Serializable]
 public class SerializableStep
 {
-    [SerializeField]
-    public List<SerializableSubstep> Substeps;
     public string description;
     public bool isCompleted;
+
+    [SerializeField]
+    public List<SerializableSubstep> Substeps;
 
     public SerializableStep(string description)
     {
@@ -167,16 +249,63 @@ public class SerializableStep
         isCompleted = Substeps.All(a => a.isCompleted == true);
         return isCompleted;
     }
+
+    override public string ToString()
+    {
+        string s = description;
+
+        foreach (var substep in Substeps)
+        {
+            s += "\n<margin=2em>" + (char)(Substeps.IndexOf(substep) + 97) + ". " + substep.description + "</margin>";
+        }
+
+        return s;
+
+    }
+
+    public string ToStringFormatted()
+    {
+        string s = "";
+
+        if (!isCompleted)
+            s += description;
+        else
+            s += "<color=green> " + description + " </color>";
+
+        foreach (var substep in Substeps)
+        {
+            if (!substep.isCompleted)
+                s += "\n<margin=2em>" + (char)(Substeps.IndexOf(substep) + 97) + ". " + substep.description + "</margin>";
+            else
+                s += "\n<margin=2em><color=green> " + (char)(Substeps.IndexOf(substep) + 97) + ". " + substep.description + " </color></margin>";
+        }
+
+        return s;
+    }
+
+    public string GetCurrentStep()
+    {
+        if (Substeps.Count == 0)
+            return description;
+
+        foreach (var s in Substeps)
+        {
+            if (!s.isCompleted)
+                return s.description;
+        }
+
+        return "Step Completed";
+    }
 }
 
 [System.Serializable]
 public class SerializableSubstep
 {
-    [SerializeField]
-    public List<GameObject> InteractionObjects;
-
     public bool isCompleted;
     public string description;
+
+    [SerializeField]
+    public List<GameObject> InteractionObjects;
 
     public SerializableSubstep(string description)
     {
@@ -185,12 +314,12 @@ public class SerializableSubstep
 
     public bool IsCompleted()
     {
-        isCompleted = InteractionObjects.All(a => a.GetComponent<Interactable>().getCompletedInteraction() == true);
+        isCompleted = InteractionObjects.All(a => a.GetComponent<Interactable>().GetCompletedInteraction() == true);
         return isCompleted;
     }
 
     public bool CheckCompletion(int index)
     {
-        return InteractionObjects[index].GetComponent<Interactable>().getCompletedInteraction();
+        return InteractionObjects[index].GetComponent<Interactable>().GetCompletedInteraction();
     }
 }
